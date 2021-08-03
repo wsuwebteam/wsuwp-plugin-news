a2696c2692771318718b7991078a43bbfc3cf538
Danial Bleile <danbleile@gmail.com>
2021-08-03 01:55:54 -0700

    Enhancement | Add news slug back into news permalink

--- includes/post-types/news-article.php
+++ includes/post-types/news-article.php
@@ -5,35 +5,35 @@ class News_Article {
 
 	private static $slug = 'news_article';
 
-    private static $attributes = array(
-        'labels' => array(
-            'name' => 'News Articles',
-            'singular_name' => 'News Article'
-        ),
-        'description' => '',
-        'public' => true,
-        'has_archive' => true,
-        'show_in_rest' => true,
-        'menu_position' => 4,
-        'menu_icon' => 'dashicons-media-document',
-        'supports' => array(
-            'title',
-            'editor',
-            'thumbnail',
-            'excerpt'
-        ),
-        'rewrite' => array(
-            'slug' => '%year%/%monthnum%/%day%',
-            'with_front' => false
-        ),
-        'taxonomies' => array(
-            'post_tag',
-            'category',
-            'author'
-        )
-    );
+	private static $attributes = array(
+		'labels' => array(
+			'name' => 'News Articles',
+			'singular_name' => 'News Article',
+		),
+		'description' => '',
+		'public' => true,
+		'has_archive' => true,
+		'show_in_rest' => true,
+		'menu_position' => 4,
+		'menu_icon' => 'dashicons-media-document',
+		'supports' => array(
+			'title',
+			'editor',
+			'thumbnail',
+			'excerpt',
+		),
+		'rewrite' => array(
+			'slug' => 'news/%year%/%monthnum%/%day%',
+			'with_front' => false,
+		),
+		'taxonomies' => array(
+			'post_tag',
+			'category',
+			'author',
+		)
+	);
 
-    public static function post_type_link($url, $post) {
+	public static function post_type_link($url, $post) {
 
 		if ( self::$slug == get_post_type($post) ) {
 			$url = str_replace( "%year%", get_the_date('Y'), $url );
@@ -45,20 +45,20 @@ class News_Article {
 
 	}
 
-    public static function register_post_type(){
+	public static function register_post_type(){
 
-        register_post_type( self::$slug, self::$attributes );
+		register_post_type( self::$slug, self::$attributes );
 
-    }
+	}
 
-    public function init() {
+	public function init() {
 
-        add_action( 'init', __CLASS__ . '::register_post_type' );
+		add_action( 'init', __CLASS__ . '::register_post_type' );
 
-        // Converts the Structure Tags in our permalink.
-        add_filter( 'post_type_link', __CLASS__ . '::post_type_link', 10, 2 );
+		// Converts the Structure Tags in our permalink.
+		add_filter( 'post_type_link', __CLASS__ . '::post_type_link', 10, 2 );
 
-    }
+	}
 
 }
 
