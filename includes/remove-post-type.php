51259685e1db263b20c1ef37a08960a35347f496
Dan White <dan.white1@wsu.edu>
2021-08-03 17:21:07 -0700

    Fix method calls

--- includes/remove-post-type.php
+++ includes/remove-post-type.php
@@ -2,15 +2,15 @@
 
 class Remove_Post_Type {
 
-	public function remove_draft_widget() {
+	public static function remove_draft_widget() {
 		remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
 	}
 
-	public function remove_default_post_type_menu_bar( $wp_admin_bar ) {
+	public static function remove_default_post_type_menu_bar( $wp_admin_bar ) {
 		$wp_admin_bar->remove_node( 'new-post' );
 	}
 
-	public function remove_default_post_type() {
+	public static function remove_default_post_type() {
 		remove_menu_page( 'edit.php' );
 	}
 
@@ -18,13 +18,13 @@ class Remove_Post_Type {
 		/*** Remove Default Post Type */
 
 		// Remove from Quick Draft
-		add_action( 'wp_dashboard_setup', array( $this, 'remove_draft_widget' ), 999 );
+		add_action( 'wp_dashboard_setup', __CLASS__ . '::remove_draft_widget', 999 );
 
 		// Remove from +New Post in Admin Bar
-		add_action( 'admin_bar_menu', array( $this, 'remove_default_post_type_menu_bar' ), 999 );
+		add_action( 'admin_bar_menu', __CLASS__ . '::remove_default_post_type_menu_bar', 999 );
 
 		// Remove from the Side Menu
-		add_action( 'admin_menu', array( $this, 'remove_default_post_type' ) );
+		add_action( 'admin_menu', __CLASS__ . '::remove_default_post_type' );
 
 	}
 
