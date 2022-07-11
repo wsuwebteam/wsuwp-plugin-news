3c76d65b075fde57152069ae3d078c3f9153e140
Danial Bleile <danbleile@gmail.com>
2022-07-11 05:50:21 -0700

    Build | Dist

--- assets/dist/index.asset.php
+++ assets/dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-url'), 'version' => 'd5f6e25fe29bbc47017535b114662957');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'e65deca43411dbca93d6');
